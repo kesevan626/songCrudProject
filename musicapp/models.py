bac4a4605b4bd0aed0bad9da8fc6fffd69d3bc30
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-10-06 17:11:39 -0700

    Second Commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='author')
+    title = models.CharField(max_length=255, null=False, blank=False)
+    likes = models.ManyToManyField(User)
+    date_released = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, verbose_name='song', related_name='song_lyrics', on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.song.title
